2cf4ac0b4bf436da7f93b50a14c9ef11df3c4f8b
ThanhQuang <quangtt@foxcode.org>
2021-01-05 23:42:00 +0700

    initial commit

--- Utils.py
+++ Utils.py
@@ -1,12 +1,13 @@
-from gensim.parsing.preprocessing import strip_non_alphanum, strip_multiple_whitespaces,preprocess_string, split_alphanum, strip_short, strip_numeric
-from sklearn.feature_extraction.text import CountVectorizer
-import string
-import re
 import codecs
-from unidecode import unidecode
-import numpy as np
+import re
+import string
 
-def load_corpus(filename):
+import numpy as np
+from gensim.parsing.preprocessing import strip_non_alphanum, split_alphanum, strip_short, strip_numeric
+from unidecode import unidecode
+
+
+def loadCorpus(filename):
     list_label = []
     list_content = []
     f = codecs.open(filename, 'r', 'utf-8')
@@ -18,17 +19,33 @@ def load_corpus(filename):
     return list_label, list_content
 
 
-#load stop words from txt
-def load_stopwordCorpus(filename):
+def removeSpecialCharacter(text):
+    output = text.lower()
+    removeLine = re.sub(r"http\S+", "", output)
+    nopunc = [char for char in removeLine if char not in string.punctuation]
+    nopunc = ''.join(nopunc)
+    doc = strip_non_alphanum(nopunc).lower().strip()  # remove emoji
+    # remove cac tu ket hop voi cac so b1,b2..
+    doctest = split_alphanum(doc)
+    document_test = strip_short(doctest, minsize=2)  # xoa cac ky tu dung 1 minh
+    remove_number_doc = strip_numeric(document_test)  # xoa cac con so
+    # rawdoc = ViTokenizer.tokenize(remove_number_doc)
+    a = unidecode(remove_number_doc)
+    return a  # ghép các từ có nghĩa trong tiếng việt
+
+
+# load stop words from txt
+def loadStopWordCorpus(filename):
     list_content = []
     f = codecs.open(filename, 'r', 'utf-8')
     for line in f:
         line = line.strip().split('\t')
         list_content.append(line[0])
-    return  list_content
+    return list_content
+
 
 # convert text in curpus to vectors
-def convertToVector(corpus,bagwods):
+def convertToVector(corpus, bagwods):
     vectors = []
     for doc in corpus:
         vector = np.zeros(len(bagwods))
@@ -38,18 +55,19 @@ def convertToVector(corpus,bagwods):
         vectors.append(vector)
     return vectors
 
+
 # map labels ham -> 0 , spam -> 1
 def mapLabelToNumber(labels):
     output = []
-    for index,label in enumerate(labels):
+    for index, label in enumerate(labels):
         if label == 'ham':
             output.append(0)
         else:
             output.append(1)
     return output
 
-# load bag_wordf from courpus
-def configure_bag_words(corpus):
+
+def exportBagWords(corpus):
     set_words = []
     for doc in corpus:
         words = doc.split(' ')
@@ -58,35 +76,22 @@ def configure_bag_words(corpus):
     set_words = list(set(set_words))
     return set_words
 
-# remove special character
-def remove_special_character(text):
-    output = text.lower()
-    removeLine = re.sub(r"http\S+", "", output)
-    nopunc = [char for char in removeLine if char not in string.punctuation]
-    nopunc = ''.join(nopunc)
-    doc = strip_non_alphanum(nopunc).lower().strip() #remove emoji
-    # remove cac tu ket hop voi cac so b1,b2..
-    doctest = split_alphanum(doc)
-    document_test = strip_short(doctest, minsize=2) # xoa cac ky tu dung 1 minh
-    remove_number_doc = strip_numeric(document_test) # xoa cac con so
-    # rawdoc = ViTokenizer.tokenize(remove_number_doc)
-    a = unidecode(remove_number_doc)
-    return a# ghép các từ có nghĩa trong tiếng việt
 
-
-def remove_stopword_from_text(message,stopwords):
-    output  = ''
+def removeStopWordFromText(message, stopwords):
+    output = ''
     words = message.split(' ')
     for word in words:
         if (word not in stopwords):
             output += word
     return output
 
-def smoothing(a, b):
-    return float((a+1)/(b+1))
 
-#get count spam or nonspam
-def getCount_Spam_or_Nonspam(labels):
+def smoothing(a, b):
+    return float((a + 1) / (b + 1))
+
+
+# get count spam or nonspam
+def getCountSpamOrNotSpam(labels):
     spam = 0
     non_spam = 0
     for l in labels:
@@ -94,15 +99,16 @@ def getCount_Spam_or_Nonspam(labels):
             spam += 1
         else:
             non_spam += 1
-    return spam,non_spam
+    return spam, non_spam
+
 
 # define bayes matrix
-def configure_bayes_matrix(bag_words,vectors,labels,spam,non_spam,bayes_matrix):
+def configureBayesMatrix(bag_words, vectors, labels, spam, non_spam, bayes_matrix):
     for i, word in enumerate(bag_words):
-        app_spam = 0 # số lần spam vectors chứa từ này
-        app_nonspam = 0 # số lần ham vectors chứa từ này
-        nonapp_spam = 0 # số lần vector spam không chứa từ này
-        nonapp_nonspam = 0 # số lần ham vectors không chứa từ này
+        app_spam = 0  # số lần spam vectors chứa từ này
+        app_nonspam = 0  # số lần ham vectors chứa từ này
+        nonapp_spam = 0  # số lần vector spam không chứa từ này
+        nonapp_nonspam = 0  # số lần ham vectors không chứa từ này
         # tổng số lần n = length(vector)
         for k, v in enumerate(vectors):
             if v[i] == 1:
@@ -116,31 +122,34 @@ def configure_bayes_matrix(bag_words,vectors,labels,spam,non_spam,bayes_matrix):
                 else:
                     nonapp_nonspam += 1
 
-        bayes_matrix[i][0] = smoothing(app_spam, spam) # p x / spam
-        bayes_matrix[i][1] = smoothing(app_nonspam, non_spam) # p x/ham
-        bayes_matrix[i][2] = smoothing(nonapp_spam, spam) # p !x/spam
-        bayes_matrix[i][3] = smoothing(nonapp_nonspam, non_spam) #p !x/ham
+        bayes_matrix[i][0] = smoothing(app_spam, spam)  # p x / spam
+        bayes_matrix[i][1] = smoothing(app_nonspam, non_spam)  # p x/ham
+        bayes_matrix[i][2] = smoothing(nonapp_spam, spam)  # p !x/spam
+        bayes_matrix[i][3] = smoothing(nonapp_nonspam, non_spam)  # p !x/ham
+
 
 # create vector from message
-def handleMessage(message,bag_words):
-    input = remove_special_character(message)
+def handleMessage(message, bag_words):
+    input = removeSpecialCharacter(message)
     vector = np.zeros(len(bag_words))
     for i, word in enumerate(bag_words):
         if word in input:
             vector[i] = 1
     return vector
 
-#compare rate spam or nonspam
+
+# compare rate spam or nonspam
 def compare(predict_spam, predict_non_spam):
-    rate_spam = predict_spam/(predict_spam+predict_non_spam)
-    rate_nonspam = predict_non_spam/(predict_spam+predict_non_spam)
+    rate_spam = predict_spam / (predict_spam + predict_non_spam)
+    rate_nonspam = predict_non_spam / (predict_spam + predict_non_spam)
     if rate_spam > rate_nonspam:
         return True
     return False
 
+
 # predict message
-def predict(message,bag_words,_p_spam,_p_nonspam,bayes_matrix):
-    vector_message = handleMessage(message,bag_words)
+def predict(message, bag_words, _p_spam, _p_nonspam, bayes_matrix):
+    vector_message = handleMessage(message, bag_words)
 
     predict_spam = _p_spam
     predict_non_spam = _p_nonspam
@@ -155,4 +164,3 @@ def predict(message,bag_words,_p_spam,_p_nonspam,bayes_matrix):
     if compare(predict_spam, predict_non_spam):
         return 1
     return 0
-
--- app.py
+++ app.py
@@ -1,46 +1,87 @@
-# This is a sample Python script.
-# ta nhận thấy trong tiếng việt thường có những dấu ở các chữ cái trên  => thay thế chữ cái
+from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.svm import LinearSVC
 
-# xóa các ký tự đặc biệt và dấu trong message tiền xử lý văn bản
-
-#mark: load corpus txt
-
-#mark: load stopword
 import Utils
-import pickle
-import time
-from flask import request
-from urllib.parse import urlparse,parse_qs
-# Press the green button in the gutter to run the script.
-from flask import Flask
-app = Flask(__name__)
+import json
+import coremltools
 
-def predict(message):
-    svm = pickle.load(open('svm', 'rb'))
-    bag_word = pickle.load(open('data', 'rb'))
-    return  svm.predict([Utils.handleMessage(message, bag_word)])[0]
-
-@app.route('/hello')
-def hello_world():
-  return 'Hello, World!'
-
-@app.route('/message')
-def message():
-    try:
-        message = request.args.get('text', type=str)
-        isSpam = predict(str(message))
-        if (isSpam == 1):
-            return 'Spam'
-        else:
-            return 'Ham'
-        return message
-    except:
-        return 'This page does not exist', 404
-
-
-
-HOST_NAME = 'localhost'
-PORT = 7979
 if __name__ == '__main__':
-  app.run()
+    print(">>>>>>>>>>>>>>>>>>>> START >>>>>>>>>>>>>>>>>>>>")
 
+    # pretreatment
+    labels, content = Utils.loadCorpus('corpus.txt')
+    corpus = [Utils.removeSpecialCharacter(d) for d in content]
+    labels = Utils.mapLabelToNumber(labels)
+    bag_word = Utils.exportBagWords(corpus)
+    vectors = Utils.convertToVector(corpus, bag_word)
+
+    # save bag words
+    # with open("sample.json", "w") as outfile:
+    #     json.dump(bag_word, outfile)
+
+    count_spam, count_non_spam = Utils.getCountSpamOrNotSpam(labels)
+    _p_spam = Utils.smoothing(count_spam, (count_non_spam + count_spam))
+    _p_non_spam = Utils.smoothing(count_non_spam, (count_non_spam + count_spam))
+
+    bayes_matrix = Utils.np.zeros((len(bag_word), 4))  # create matrix
+    Utils.configureBayesMatrix(bag_word, vectors, labels, count_spam, count_non_spam, bayes_matrix)
+    spam = _p_spam
+    not_spam = _p_non_spam
+
+    # train model using naive bayes
+    clf1 = BernoulliNB(alpha=1e-10)
+    clf1.fit(vectors, labels)
+
+    # train model using svm
+    svm = LinearSVC()
+    svm.fit(vectors, labels)
+
+    # test
+    test_texts = [
+        'Cho vay tieu dùng ca nhan,Lai xuat hap hap dan,lien he 099.666.8888',
+        'Rảnh không mi, đi học bơi với t nè, chừ luôn',
+        'Em đi học về chưa',
+        'Gọi ngay, còn duy nhất 20 căn giá rẻ. ĐT xxxxxxx',
+        'Ban duoc FE CREDIT ho tro vay den 50 TRIEU VND, tra gop tu 439,000d/thang…',
+        'Nếu mà cô quyết định như này thì tối qua anh không để em một mình đâu',
+        'Bạn đang làm gì đấy, inbox mình nhờ chút chuyện với nhé',
+        'Chương trình khuyến mãi lớn nhất trong năm, giảm giá 50% tất cả các mặt hàng, free quẹt thẻ, 100% mua là trúng'
+    ]
+
+    test_labels = [1, 0, 0, 1, 1, 0, 0, 1]
+    x_tests = [Utils.handleMessage(text, bag_word) for text in test_texts]
+
+    examplePredict = clf1.predict(x_tests)
+    examplePredict2 = svm.predict(x_tests)
+
+    print("Bernoulli ex", examplePredict)
+    print('Bernoulli: Training size = %d, accuracy = %.2f%%' % \
+          (len(vectors), accuracy_score(test_labels, examplePredict) * 100))
+
+    print("Svm ex", examplePredict2)
+    print('Svm: Training size = %d, accuracy = %.2f%%' % \
+          (len(vectors), accuracy_score(test_labels, examplePredict2) * 100))
+
+    # smvv = [svm.predict([Utils.handleMessage(d, bag_word)])[0] for d in corpus]
+    # preds = [clf1.predict([Utils.handleMessage(d, bag_word)])[0] for d in corpus]
+    # pred = [Utils.predict(d, bag_word, spam, not_spam, bayes_matrix) for d in corpus]
+    # print(accuracy_score(labels, smvv) * 100)
+    # print(accuracy_score(labels, pred) * 100)
+    # print('%', accuracy_score(labels, preds) * 100)
+
+    # create coreML model
+
+    # svmModel = coremltools.converters.sklearn.convert(svm, "message", 'label')
+    # svmModel.author = 'Thanh Quang'
+    # svmModel.short_description = "Classify whether message is spam or not"
+    # svmModel.input_description["message"] = "vector of message to be classified"
+    # svmModel.output_description["spam_or_not"] = "Whether message is spam or not"
+    # svmModel.save('detect_spam.mlmodel')
+
+    # coreml_model = coremltools.converters.sklearn.convert(clf1)
+    # coreml_model.author = 'Thanh Quang'
+    # coreml_model.short_description = "Classify whether message is spam or not"
+    # coreml_model.input_description["message"] = "vector of message to be classified"
+    # coreml_model.output_description["spam_or_not"] = "Whether message is spam or not"
+    # coreml_model.save("detect_spam.mlmodel")
